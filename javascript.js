1d5d587c587c1816d33d096291d7e5f2f390461b
NightFun <82947394+NightFunn@users.noreply.github.com>
2024-12-22 06:39:15 +0700

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Chào mừng đến với trang profile TikTok của streamer!");
+    // Thêm các tính năng JavaScript khác nếu cần
+});
